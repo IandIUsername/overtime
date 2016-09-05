d5c67ca03a3ee385157f69267a1f0e571f85b20b
theshell <z89quark@gmail.com>
2016-09-05 23:39:22 +0000

    create action and form, show action and page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,33 @@
 class PostsController < ApplicationController
     
+    #before_action: :find_post, only: (:index, :create, :show, :edit, :destroy)
+    
     def index
     
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def create
+       @post = Post.create(post_params)
+       @post.save
+       redirect_to post_path(@post)
+    end
+    
+    def show
+      @post = Post.find(params[:id])  
+    end
+    
+    private 
+    
+    def post_params
+        params.require(:post).permit(:date, :rationale)
+    end
+    
+    def find_post
+       @post = Post.find(params[:id]) 
+        
     end
 end
