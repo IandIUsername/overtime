2af08617dd0ad0352e4058aa0e7bb72d3b9f7374
theshell <z89quark@gmail.com>
2016-09-05 20:33:19 +0000

    resource generator post and post model testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903220939) do
+ActiveRecord::Schema.define(version: 20160904001046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20160903220939) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
+    t.date     "date"
+    t.text     "rationale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
