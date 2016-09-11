086d5a2864a51ef98d24b1f0d9cc9feabdb86f0d
theshell <z89quark@gmail.com>
2016-09-11 23:48:28 +0000

    approval workflow radio buttons on form

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 Post.delete_all
 
 puts "all posts deleted"
+
 User.delete_all
+
 puts "all users deleted"
 
 @user = User.create(email: "dogs@dogs.com", password: "password", password_confirmation: "password", first_name: "Doogie", last_name: "Howser")
 
-puts "1 user created
-"
+puts "1 user created"
+
+@adminuser = AdminUser.create(email: "admin@putzesrus.com", password: 'password', password_confirmation: 'password', first_name: 'AdminBill', last_name: 'AdminExcellent-adventure')
+
+puts "1 adminuser created"
+
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{Post} rationale content", user_id: @user.id)
 end
