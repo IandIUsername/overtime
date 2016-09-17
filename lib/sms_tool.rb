a2d19f54731e32037b7fe0a26837584a0c2d6fa2
theshell <z89quark@gmail.com>
2016-09-17 00:22:31 +0000

    implemeted sms tool in lib

---
+++ lib/sms_tool.rb
@@ -0,0 +1,12 @@
+
+
+
+    module SmsTool
+        def self.send_sms(number:, message:)
+            puts "sending sms..."
+            puts "#{message} to #{number}"
+            
+        end
+    
+    end
+
