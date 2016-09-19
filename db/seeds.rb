82a6d730805e70d3bc037731352d4af0a6c67ab5
theshell <z89quark@gmail.com>
2016-09-19 04:04:08 +0000

    updated the audit_log factory and tested out six day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,16 @@ User.delete_all
 
 puts "all users deleted"
 
+AuditLog.delete_all
+
+puts "all auditlogs deleted"
+
 @user = User.create(email: "dogs@dogs.com", 
 password: "password", 
 password_confirmation: "password", 
 first_name: "Doogie", 
 last_name: "Howser", 
-phone: "555-1212")
+phone: "1234567890")
 
 puts "1 user created"
 
@@ -20,7 +24,7 @@ password: 'password',
 password_confirmation: 'password', 
 first_name: 'AdminBill', 
 last_name: 'AdminExcellent-adventure', 
-phone: '444-3322')
+phone: '1234567890')
 
 puts "1 adminuser created"
 
@@ -28,4 +32,13 @@ puts "1 adminuser created"
     Post.create!(date: Date.today, rationale: "#{Post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
+
 puts "100 posts have been created"
+
+
+100.times do |audit_log|
+    AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days) )
+end
+
+
+puts "100 auditlogs have been created"
