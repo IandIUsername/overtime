396c23f86609f04deadfe258f6805c4df60b69cb
theshell <z89quark@gmail.com>
2016-09-20 00:45:42 +0000

    implemented audit log tab that is shown only to admin users

---
+++ app/policies/audit_log_policy.rb
@@ -0,0 +1,17 @@
+class AuditLogPolicy < ApplicationPolicy
+    
+    
+    def index?
+        return true if admin?
+    end
+    
+    
+    private
+    
+    def admin?
+       admin_types.include?(user.type) 
+    end
+    
+    
+    
+end
