b77514e19dba300c91ed3dc5ba6eb7f5fda4c45c
theshell <z89quark@gmail.com>
2016-09-25 03:29:05 +0000

    audit log prevent unauthorized access pundit

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -25,7 +25,13 @@ describe "AuditLogFeature" do
           
       end
       
-      xit "cannot be accessed by non admin users" do
+      it "cannot be accessed by non admin users" do
+         logout(:user)
+         user = FactoryGirl.create(:user)
+         login_as(user, :scope => :user)
+         
+         visit audit_logs_path
+         expect(current_path).to eq(root_path)
          
          
       end
